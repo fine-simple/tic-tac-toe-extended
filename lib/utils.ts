f62730798ff71dc28459a2937de4dc9ea14a5d18
Ahmed Tawfik <ahmed.tawfik.at10@gmail.com>
2025-01-03 18:27:00 +0200

    Refactor calculateWinner to be more readable

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,19 +6,22 @@ export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
 
-export const calculateWinner = (squares: Board): Winner | null => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+const winningPatterns = [
+  // horizontal
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  // vertical
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  // diagonal
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-  for (const [a, b, c] of lines) {
+export const calculateWinner = (squares: Board): Winner | null => {
+  for (const [a, b, c] of winningPatterns) {
     if (
       squares[a] &&
       squares[a] !== "draw" &&
@@ -29,7 +32,7 @@ export const calculateWinner = (squares: Board): Winner | null => {
     }
   }
 
-  if (squares.every((cell) => cell !== null)) {
+  if (squares.every(cell => cell !== null)) {
     return "draw";
   }
 
